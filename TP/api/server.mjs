af9ec1704ec1aea0f4eca90068c26073de85f2b3
ANTOINE Mathieu <Mathieu.Antoine@bordeaux-inp.fr>
2024-10-15 16:18:00 +0200

    probleme avec l'api

--- TP/api/server.mjs
+++ TP/api/server.mjs
@@ -1,11 +1,13 @@
 import Fastify from "fastify";
+import { featureStore } from "./featureStore.mjs";
 
 export function createServer() {
   const fastify = Fastify();
 
-  fastify.get("/feature/:code", function(req,res){
-    const code =req.params.code;
-    res.send({"code": code, "enabled": true});
+  fastify.get("/feature/:code", async function handler(request, reply) {
+    const { code } = request.params;
+    const enabled = await featureStore.isEnabled(code);
+    return reply.send({ code, enabled });
   });
 
   return fastify;
